16533245f370befab42a488361c9ca6f318de10b
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2021-09-22 11:51:28 -0300

    feat: responsividade no navBar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 import CardNav from "../CardNav";
+
 import { Container } from "./styles";
 
 export default function SideBar() {
+  const [toogleBar, setToogleBar] = useState(false);
+
+  const openBar = () => setToogleBar(!toogleBar);
+
   return (
     <Container>
       <nav>
@@ -10,7 +17,24 @@ export default function SideBar() {
           <div></div>
         </div>
 
-        <ul>
+        <ul className={`${toogleBar ? "visible" : ""}`}>
+          <button className="btn-mobile" onClick={openBar}>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="times"
+              className="svg-inline--fa fa-times fa-w-11"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 352 512"
+            >
+              <path
+                fill="currentColor"
+                d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+              ></path>
+            </svg>
+          </button>
           <CardNav
             routers="/"
             text="Home"
@@ -96,6 +120,24 @@ export default function SideBar() {
             }
           />
         </ul>
+
+        <button className="btn-mobile menu-mobile" onClick={openBar}>
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="bars"
+            className="svg-inline--fa fa-bars fa-w-14"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path
+              fill="currentColor"
+              d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+            ></path>
+          </svg>
+        </button>
       </nav>
     </Container>
   );
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -27,13 +27,34 @@ export const Container = styled.header`
         border-radius: 8px;
       }
     }
-    
+
+    .btn-mobile {
+      opacity: 0;
+      visibility: hidden;
+      display: none;
+      cursor: pointer;
+      background-color: transparent;
+      border: none;
+      svg {
+        color: white;
+      }
+    }
+
     ul {
       display: flex;
       align-items: center;
       justify-content: center;
       li + li {
         margin-left: 1rem;
+      }
+      .btn-mobile {
+        position: absolute;
+        top: 5%;
+        left: 0.3rem;
+        svg {
+          width: 1.7rem;
+          height: 1.7rem;
+        }
       }
     }
     a {
@@ -60,6 +81,46 @@ export const Container = styled.header`
       &:hover::after {
         transform: scale(1);
         transform-origin: bottom left;
+      }
+    }
+
+    @media (max-width: 760px) {
+      ul {
+        position: fixed;
+        top: 0;
+        right: 0;
+        height: 100%;
+        transform: translateX(100%);
+        transition: transform 0.3s ease-in-out;
+
+        flex-direction: column;
+        justify-content: flex-start;
+        padding: 4.8rem 1rem 0;
+
+        background-color: ${props => props.theme.colors.backgroundSecondary};
+        li + li {
+          margin-top: 3rem;
+          margin-left: 0;
+        }
+
+        &.visible {
+          transform: translateX(0);
+        }
+        .btn-mobile {
+          opacity: 1;
+          visibility: visible;
+          display: inline-block;
+        }
+      }
+
+      .btn-mobile {
+        opacity: 1;
+        visibility: visible;
+        display: inline-block;
+        svg {
+          width: 2rem;
+          height: 2rem;
+        }
       }
     }
   }
